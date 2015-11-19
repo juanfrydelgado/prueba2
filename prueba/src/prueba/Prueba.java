e28f215b81ee03e3226267e7c70f6bedd65ddd5d
juanfrydelgado <juanfrydelgado@gmail.com>
2015-11-19 14:16:49 +0100

    Update Prueba.java

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("primera linea");
+		System.out.println("escrito desde el remoto");
 	}
 
 }
